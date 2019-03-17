d04306c9c9fea1770c955a5b62728e74a2b78f28
Cofel <704328580@qq.com>
2019-03-17 19:53:27 +0800

    Spring Boot Demo By Sunny Version:1.0

---
+++ sunny-starter-core/src/main/java/com/cofel/core/base/BaseController.java
@@ -0,0 +1,7 @@
+package com.cofel.core.base;
+
+/**
+ * Controller集成本类可以获得一些基本方法
+ */
+public class BaseController {
+}
---
+++ sunny-starter-core/src/main/java/com/cofel/core/base/BaseService.java
@@ -0,0 +1,7 @@
+package com.cofel.core.base;
+
+/**
+ * 所有的Service集成本类可以获得一些基本方法
+ */
+public class BaseService {
+}
