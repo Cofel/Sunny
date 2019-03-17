2fff3b27c0fed2b6e6ae2326a422c9bcedcd073b
Cofel <704328580@qq.com>
2019-03-17 16:16:27 +0800

    Spring Boot Demo By Sunny Version:1.0

---
+++ sunny-starter-cache/src/main/java/com/cofel/cache/SunnyStarterCacheApplication.java
@@ -0,0 +1,13 @@
+package com.cofel.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunnyStarterCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunnyStarterCacheApplication.class, args);
+    }
+
+}
---
+++ sunny-starter-security/src/main/java/com/cofel/security/SunnyStarterSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.cofel.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunnyStarterSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunnyStarterSecurityApplication.class, args);
+    }
+
+}
